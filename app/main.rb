50fdaceb8671ed15dfd8c3b1d20586db10ccf015
Apprentice <apprentice@dbc04.local>
2013-04-21 20:21:40 -0700

    more interface work

--- app/main.rb
+++ app/main.rb
@@ -1,20 +1,44 @@
+require '../db/config'
 class Interface
+  
+  attr_reader :command, :option
+
   def initialize
-    welcome
-    gets_input
+    # welcome
+    # menu
+    # gets_input
+    find(gets.chomp)
   end
 
 
-  def welcome
-    puts "Welcome to Sunshine Congress"
+  # def welcome
+  #   puts "Welcome to Sunshine Congress"
+  # end
+
+  # def menu
+  #   puts "Type one: search | list"
+  #   puts "Followed by the politician
+  #   name"
+  #   puts "(e.g., search Dianne Feinstein)"
+  # end
+
+  # def gets_input
+  #   @command = gets.chomp[0]
+  #   @option = gets.chomp[1..-1]
+  # end
+
+  # def query
+  #   if @command == 'search'
+  #     find(@option)
+  #   else
+  #     puts "didn't work"
+  #   end
+  # end
+
+  def find(num)
+    Politician.find(num)
   end
 
-  def menu
-    puts "Welcome to Sunshine Congress"
-  end
+end
 
-  def gets_input
-    command = gets.chomp[0]
-    option = gets.chomp[1]
-    if 
-  end
+Interface.new
